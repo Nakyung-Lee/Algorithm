66c75fe053272a6fda85642c45370dccd3713fc6
Nakyung-Lee <68135400+Nakyung-Lee@users.noreply.github.com>
2022-05-25 09:11:17 +0900

    이진탐색 (재귀, 반복)

---
+++ coding/탐색/이진탐색(재귀).py
@@ -0,0 +1,40 @@
+# 이진 탐색 소스코드 구현 (재귀함수)
+
+def binary_search(array, target, start, end):
+    if stard > end:
+        return None
+    mid = (start + end) // 2
+    #찾은 경우 중간점 인덱스 반환
+    if array[mid] == target:
+        return mid
+    elif array[mid] > target:
+        return binary_search(array, target, start, mid - 1)
+    else:
+        return binary_search(array, target, mid + 1, end)
+
+# 이진 탐색 소스코드 구현 (반복문)
+
+def binary_search(array, target, start, end):
+    while start <= end:
+        mid = (start + end) // 2
+        if array[mid] == target:
+            return mid
+        elif array[mid] > target :
+            end = mid -1
+        else:
+            start = mid + 1
+    return None
+  
+  
+  
+n, target = list(map(int,input().split()))
+array = list(map(int,input().split()))
+
+result = binary_search(array, target, 0, n - 1)
+
+if result == None:
+    print("원소가 존재하지 않는다.")
+else:
+    print(result + 1)
+    
+ 
