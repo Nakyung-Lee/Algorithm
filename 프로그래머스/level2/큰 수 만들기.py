990813fbd80ebb18848e06b24d510b81bd50e43b
Nakyung-Lee <68135400+Nakyung-Lee@users.noreply.github.com>
2022-08-09 23:12:37 +0900

    stack 활용

--- 프로그래머스/level2/큰 수 만들기.py
+++ 프로그래머스/level2/큰 수 만들기.py
@@ -16,4 +16,23 @@ def solution(number, k):
   ----------------------------------------------
   #시간초과 발생
   
+  #stack 사용 pop
   ----------------------------------------------
+
+def solution(number, k):
+    answer = [] # Stack 
+    
+    for num in number:
+        if not answer:
+            answer.append(num)
+            continue
+        if k > 0:
+            while answer[-1] < num:
+                answer.pop()
+                k -= 1
+                if not answer or k <= 0:
+                    break
+        answer.append(num)
+        
+    answer = answer[:-k] if k > 0 else answer
+    return ''.join(answer)
